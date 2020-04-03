0ff0e7c60cb3060799fee6966fb0926faed007c7
Nicholas Feitel <Nicholas.Feitel@gmail.com>
2020-04-03 09:07:12 -0400

    br

--- src/pages/about.js
+++ src/pages/about.js
@@ -37,6 +37,7 @@ const About = () => {
             <h1>About this author:</h1>
             {/* <img style={{maxWidth: "80%", height: "auto"}} alt="me" src={headshot}/> */}
             <Img alt="headshot" fluid={data.file.childImageSharp.fluid} />
+            <br/>
             <p>Nicholas Feitel is a NYC-based full-stack web developer, working in React/Redux, JavaScript, HTML5, SQL, Python, CSS, Node, Ruby/Rails, Gatsby.js and whatever other cool code he can get his hands on.
                         Before coming to tech, he was a modern-dance critic, an animal-shelter volunteer, the artistic director of a comedy theater and, briefly, a reality televison personality. 
                         He is a proud graduate of the Flatiron School/Access Labs program and NYU's Kanbar Institute of Film/TV. He has not met your dog yet, but is sure that he will love them.</p>
